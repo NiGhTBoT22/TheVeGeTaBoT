10c7a4027fc63979e17dc2401cf7ec79df84c180
NiGhT_BoT_22 <88623043+NiGhTBoT22@users.noreply.github.com>
2021-10-19 19:48:11 +0530

    Create imagefiles.py

---
+++ TheVegetaBot/resources/imagefiles.py
@@ -0,0 +1,6 @@
+# This File Contains all the Picture Links used all over the Bot
+
+#Images in __main__.py
+VeGeTa_IMG = "https://telegra.ph/file/1bcec7b7230ea39ad9166.mp4"
+VeGeTa_START_IMG = "https://telegra.ph/file/273496a28bd1b9fc26b96.jpg"
+VeGeTa_HELP_IMG = "https://telegra.ph/file/9296a03c061cb346efdb6.jpg"
